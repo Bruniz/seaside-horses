7b0095b5d7dbccd50671b4b96f838912847554fa
Mathias Brunström <mathiasbrunstrom@gmail.com>
2017-06-26 20:30:23 +0300

    Initial project commit
    Demo shell application running

---
+++ src/fire.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD4-sz6n45Z66ZDLdi2FLGpGzO2eE-sBWc",
+    authDomain: "seaside-horses.firebaseapp.com",
+    databaseURL: "https://seaside-horses.firebaseio.com",
+    projectId: "seaside-horses",
+    storageBucket: "seaside-horses.appspot.com",
+    messagingSenderId: "698696117777"
+};
+var fire = firebase.initializeApp(config);
+export default fire;
