3e30ffea4b69159f0a92fce5187f10248381424d
Mathias Brunström <mathiasbrunstrom@gmail.com>
2018-03-24 17:12:31 +0200

    Style should work

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ import {renderToString} from 'react-dom/server';
 import {StaticRouter} from 'react-router-dom';
 import App from './src/App';
 import express from 'express';
-import './css/w3.css';
-import './css/index.css';
 
 const app = express();
-
+const staticContext = {};
 app.get('**', (req, res) => {
-    const html = renderToString(<StaticRouter><App/></StaticRouter>);
+    const html = renderToString(<StaticRouter context={staticContext}><App/></StaticRouter>);
 
     //res.set('Cache-Control', 'public, max-age=600, s-maxage=1200');
     res.send(`
@@ -19,6 +17,8 @@ app.get('**', (req, res) => {
             <head>
                 <meta charset="UTF-8">
                 <title>Seaside Horses</title>
+                <link rel="stylesheet" type="text/css" href="/css/index.css">
+                <link rel="stylesheet" type="text/css" href="/css/w3.css">
             </head>
             <body>
                 <div id="root">${html}</div>
